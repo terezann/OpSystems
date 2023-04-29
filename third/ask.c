cc3a14c3d14d460fb1be5b3bdef7fad8a78f7781
terezann <4tereza@gmail.com>
2023-04-29 13:55:07 +0300

    to do: multiple kids

--- third/ask.c
+++ third/ask.c
@@ -3,114 +3,141 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/select.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 #define max(x, y) (((x) > (y)) ? (x) : (y))
 #define BUFSIZE 256
 
 int main(int argc, char *argv[]) {
+    int pid_array[100];
     int pipe1fd[2];
     int pipe2fd[2];
-    char buffer[BUFSIZE];
-    // fd_set writefds;
+    char buffer1[BUFSIZE];
+    char buffer2[BUFSIZE];
+    char buffer_c[BUFSIZE];
     fd_set readfds;
     int maxfd;
+    int n = atoi(argv[1]);
+
+    for (int i = 0; i < n; i++) {
+        int pipe1fd[i];
+    }
 
     if(pipe(pipe1fd) < 0 || pipe(pipe2fd) < 0) {
-        perror("pipe error");
+        perror("pipe\n");
         exit(EXIT_FAILURE);
     }
 
-    pid_t pid = fork(); //ftiakse ena paidi
 
-    if (pid < 0) { //elegxos paidiou
-        perror("fork error");
-        exit(EXIT_FAILURE);
-    }
-
-    if (pid == 0){ //child process
-        // int val;
-        int gIndex = 1; //number of child
-        while(1) {
-
-            int n1 = read(pipe1fd[0], buffer, sizeof(int));
-            if (n1 <= 0) {
-                perror("child read to pipe error\n");
-                exit(EXIT_FAILURE);
-            }
-            int num = atoi(buffer);
-            printf("[Child %d] [%d] Child received %d!\n", gIndex, getpid(), num);
-            num++;
-
-            sleep(5);
-
-            int n2 = write(pipe2fd[1], &num, sizeof(int));
-            if (n2 <= 0) {
-                perror("child write to pipe error\n");
-                exit(EXIT_FAILURE);
-            }
-            printf("[Child %d] [%d] Child finished hard work, writing back %d", gIndex, getpid(), num);
-
+    for (int i = 0; i < n; i++) {
+        pid_t pid = fork();
+        pid_array[i] = pid;
+        if (pid < 0) {
+            perror("fork\n");
+            exit(EXIT_FAILURE); 
         }
         
-    }
+        if (pid == 0) { 
+            while (1) {
+                int gIndex = i;
+                int n1 = read(pipe1fd[0], buffer_c, sizeof(int));
 
-    //parent process
+                if (n1 <= 0) {
+                    perror("child read to pipe\n");
+                    exit(EXIT_FAILURE);
+                }
+
+                int num = atoi(buffer_c);
+                printf("[Child %d] [%d] Child received %d!\n", gIndex, getpid(), num);
+                num++;
+
+                sleep(5);
+
+                sprintf(buffer_c, "%d", num);
+                // strcpy(buffer2, itoa(num));
+
+                int n2 = write(pipe2fd[1], buffer_c, sizeof(int));
+                if (n2 <= 0) {
+                    perror("child write to pipe\n");
+                    exit(EXIT_FAILURE);
+                }
+                printf("[Child %d] [%d] Child finished hard work, writing back %d\n", gIndex, getpid(), num);
+            }
+        }
+
+    }
+    
+    //else --> parent process
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+    FD_SET(pipe2fd[0], &readfds);
+    maxfd = max(pipe2fd[0], STDIN_FILENO);
     while(1) {
-        FD_ZERO(&readfds);
-        FD_SET(STDIN_FILENO, &readfds);
-        FD_SET(pipe2fd[0], &readfds);
-        maxfd = max(pipe2fd[0], STDIN_FILENO);
 
         if (select(maxfd + 1, &readfds, NULL, NULL, NULL) < 0) {
-            perror("select error");
+            perror("select\n");
             continue;
         }
-
+        
         if (FD_ISSET(STDIN_FILENO, &readfds)) {
             //read data from stdin
-            int n = read(STDIN_FILENO, buffer, BUFSIZE); //thelei akrivws oso einai to noumero alla tha to ftiaksw se 2h fash.
+            int n = read(STDIN_FILENO, buffer1, BUFSIZE); //thelei akrivws oso einai to noumero alla tha to ftiaksw se 2h fash.
 
-            buffer[n] = '\0';
+            buffer1[n] = '\0';
 
             if (n <= 0) {
-                perror("read from stdin error");
+                perror("read from stdin\n");
                 exit(EXIT_FAILURE);
             }
 
-            if (n > 0 && buffer[n-1] == '\n') {
-                buffer[n-1] = '\n';
+            if (n > 0 && buffer1[n-1] == '\n') {
+                buffer1[n-1] = '\0';
             }
 
-            printf("Received input from stdin: %s\n", buffer);
+            if (n >= 4 && strncmp(buffer1, "exit", 4) == 0) {
+                // user typed 'exit', kill child and exit properly
+                for (int i = 0; i < atoi(argv[1]); i++) {
+                    kill(pid_array[i], SIGTERM);
+                    printf("Waiting for child %d\n", i);
+                }
+                exit(EXIT_SUCCESS);
+            }
 
-            //write user's input to pipe1
-            if (write(pipe1fd[1], buffer, BUFSIZE) < 0) {
-                perror("write to pipe1 error\n");
+            printf("Received input from stdin: %s\n", buffer1);
+
+            //write user's input to the suitable pipe.
+            //estw seiriaka, tha ginei meta to random.
+
+            if (write(pipe1fd[1], buffer1, BUFSIZE) < 0) {
+                perror("write to pipe1\n");
                 exit(EXIT_FAILURE);
             }
-            printf("Assigned %s to child\n", buffer);
-
+            else {
+                printf("Assigned %s to child\n", buffer1);
+            }
         }
 
-        if (FD_ISSET(pipe2fd[0], &readfds)) {
-            int n = read(pipe2fd[0], buffer, BUFSIZE);
-            if (n <= 0) {
-                perror("read from pipe error");
-                exit(EXIT_FAILURE);
+        for (int i = 0; i < atoi(argv[1]); i++) {
+            if (FD_ISSET(pipe2fd[0], &readfds)) {
+                int n = read(pipe2fd[0], buffer2, sizeof(int));
+                if (n <= 0) {
+                    perror("read from pipe\n");
+                    exit(EXIT_FAILURE);
+                }
+                printf("Received message from child nolala: %s\n", buffer2);
             }
-            printf("Received message from child nolala: %s\n", buffer);
         }
 
-        int n = write(pipe1fd[1], buffer, BUFSIZE);
+        int n = write(pipe1fd[1], buffer1, BUFSIZE);
         if (n < 0) {
-            perror("write to pipe error");
+            perror("write to pipe\n");
             exit(EXIT_FAILURE);
         }
         printf("Writing to the child?\n");
         sleep(2);
 
-
-        // write(pipe1fd[1], fds);
     }
     
 }
